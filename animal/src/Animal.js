cf5d69e77928b8e553f2dac10d70c0d3bdf0238e
unkmitm <unkmitm@gmail.com>
2025-05-16 10:16:24 +0330

    images added

--- animal/src/Animal.js
+++ animal/src/Animal.js
@@ -1,5 +1,26 @@
-function Animal () {
+import bird from "../images/bird";
+import cat from "../images/cat";
+import cow from "../images/cow";
+import dog from "../images/dog";
+import gator from "../images/gator";
+import horse from "../images/horse";
+import heart from "../images/heart";
 
+const animalObj = {
+    bird , 
+    cat , 
+    cow , 
+    dog ,
+    gator , 
+    horse
+};
+
+function Animal ({type}) {
+    return (
+        <div>
+            {type}
+        </div>
+    )
 }
 
 export default Animal;
--- animal/src/App.js
+++ animal/src/App.js
@@ -1,7 +1,32 @@
 import Animal from "./Animal";
+import { useState } from "react";
+
+function randomAnimalGenerator () {
+    const animals = ['cat' , 'dog' , 'cow' , 'bird' , 'aligator'];
+    const randoms = Math.floor(Math.random() * animals.length);
+    
+    return animals[randoms];
+};
 
 function App () {
-    
+
+    const [animal , animalAdd] = useState([]);
+
+    const clickHandler = () => {
+        return animalAdd([...animal , randomAnimalGenerator()])
+    };
+
+    const animalLists = animal.map((animals , index)=>{
+        return <Animal type={animals} key={index}/>
+    });
+
+    return (  
+    <div>
+        <button onClick={clickHandler}>Add Animal</button>
+        <div>{animalLists}</div>
+    </div>
+
+    )
 }
 
 export default App;
